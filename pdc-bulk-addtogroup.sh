7ee48f98d96b086cd8816a28312291a609287cc7
Douglas Barone <douglasvbarone@gmail.com>
2022-07-25 16:15:22 -0400

    Updated scripts

---
+++ pdc-bulk-addtogroup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+file=$1
+group=$2
+
+# Check if arguments are not empty
+if [ -z "$file" ]; then
+  echo "Usage: $0 <file>"
+  exit 1
+fi
+
+# Check if file exists
+if [ ! -f "$file" ]; then
+  echo "File $file does not exist"
+  exit 1
+fi
+
+# Check if file is empty
+if [ ! -s "$file" ]; then
+  echo "File $file is empty"
+  exit 1
+fi
+
+while IFS="" read -r p || [ -n "$p" ]; do
+  USERNAME=$(echo $p | cut -d ";" -f 1)
+  
+  # Check if $USERNAME is empty
+  if [ -z "$USERNAME" ]; then
+    echo "Username is empty"
+    continue
+  fi
+
+  echo "---"# Add user to group alunos
+  echo "Adding user $USERNAME to group $group"
+  samba-tool group addmembers $group $USERNAME
+done <$file
+
+
+
+
+
+
+
+
+
